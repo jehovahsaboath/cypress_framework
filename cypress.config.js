4913ff80b3d88dced9fe4c57696b3bfd9b20f82a
jehovahsaboath <etinosa4good@yahoo.com>
2026-07-14 02:49:24 +0400

    yml additions

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,19 @@
-const { defineConfig } = require("cypress");
+
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+})
+
